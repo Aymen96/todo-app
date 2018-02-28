2fd5aed5d7ee916f225f024f489421f584843c29
Aymen Hachicha <aymenhachicha96@gmail.com>
2018-02-28 10:03:12 +0100

    added style to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {TodoDataService} from './todo-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   providers:[TodoDataService]
 })
 export class AppComponent {

f516807d632c7939be232e8c3fd6065d3cb12fd9
Aymen Hachicha <aymenhachicha96@gmail.com>
2018-03-01 22:29:25 +0100

    added version 2 of the project TO BE EDITTED SOON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,59 @@
-import { Component } from '@angular/core';
-import {Todo} from './todo';
-import {TodoDataService} from './todo-data.service';
-
+import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers:[TodoDataService]
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-	newTodo: Todo = new Todo();
+  todos: Todo[] = [];
 
-  constructor(private todoDataService: TodoDataService) {
-  this.todoDataService = todoDataService;
+  constructor(
+    private todoDataService: TodoDataService
+  ) {
   }
 
-addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
+  public ngOnInit() {
+    this.todoDataService
+      .getAllTodos()
+      .subscribe(
+        (todos) => {
+          this.todos = todos;
+        }
+      );
   }
 
-  toggleTodoComplete(todo) {
-    this.todoDataService.toggleTodoComplete(todo);
+  onAddTodo(todo) {
+    this.todoDataService
+      .addTodo(todo)
+      .subscribe(
+        (newTodo) => {
+          this.todos = this.todos.concat(newTodo);
+        }
+      );
   }
 
-  removeTodo(todo) {
-    this.todoDataService.deleteTodoById(todo.id);
+  onToggleTodoComplete(todo) {
+    this.todoDataService
+      .toggleTodoComplete(todo)
+      .subscribe(
+        (updatedTodo) => {
+          todo = updatedTodo;
+        }
+      );
   }
 
-  get todos() {
-    return this.todoDataService.getAllTodos();
+  onRemoveTodo(todo) {
+    this.todoDataService
+      .deleteTodoById(todo.id)
+      .subscribe(
+        (_) => {
+          this.todos = this.todos.filter((t) => t.id !== todo.id);
+        }
+      );
   }
 }
